b545bc511c79c18648f3e75c1e2696e5a34e344a
Ivan <koovalin@gmail.com>
2022-12-04 01:56:21 +0300

    update 06 and 07

---
+++ Seminar_06/Program.cs
@@ -0,0 +1,129 @@
+﻿// // 
+
+// int[] array = GetArray(10, 0, 10);
+// Console.WriteLine($"[{String.Join(", ", array)}]");
+
+// int[] result = ReverseArray2(array);
+// Console.WriteLine($"[{String.Join(", ", result)}]");
+
+// ReverseArray1(array);
+// Console.WriteLine($"[{String.Join(", ", array)}]");
+
+
+// int[] GetArray(int size, int minValue, int maxValue){
+//     int[] array = new int[size];
+//     for(int i = 0; i < size; i++){
+//         array[i] = new Random().Next(minValue, maxValue+1);
+//     }
+//     return array;
+// }
+
+// void ReverseArray1 (int[] array){
+//     for(int i = 0; i < array.Length/2; i++){
+//         int temp = array[i];
+//         array[i] = array[array.Length - i - 1];
+//         array[array.Length - i - 1] = temp;
+//     }
+// }
+
+// int[] ReverseArray2 (int[] array){
+//     int[] result = new int[array.Length];
+//     for(int i = 0; i < array.Length; i++){
+//         result[i] = array[array.Length - i - 1];
+//     }
+//     return array;
+// }
+
+
+
+
+
+// Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+
+
+// int A, B, C;
+// Console.WriteLine("Введи сторону А:");
+// A = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введи сторону B:");
+// B = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введи сторону C:");
+// C = int.Parse(Console.ReadLine()!);
+// if (Treugol(A, B, C))
+//     Console.WriteLine("Такой треугольник существует!");
+// else
+//     Console.WriteLine("Такой треугольник не существует!");
+
+
+// bool Treugol(int A, int B, int C)
+// {
+//     if ((A + B) > C  && (A + C) > B && (C + B) > A)
+//         return true;
+//     return false;
+// }
+
+
+
+
+
+
+// // Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// // Делится число на 2 и записываются его остатки с конца в начало
+
+
+// int num10;
+// Console.WriteLine("Введи десятичное число: ");
+// num10 = int.Parse(Console.ReadLine()!);
+
+
+
+// int From10To2(int num10){
+
+//     while (num10 != 0){
+//         int n = num10 / 2;
+//         Console.Write(n);
+//     }
+// }
+
+
+
+// **Задача 44:** Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+
+// int n = 7;
+// int first = 0;
+// int second = 1;
+// Console.Write($"{first} {second}");
+
+// for (int i = 0; i < n-2; i++){
+//     int num = 0;
+//     num = first + second;
+//     Console.Write($" {num}");
+//     first = second;
+//     second = num;
+// }
+
+
+// Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+int[] arr = GetArray(10);
+
+Console.WriteLine($"[{String.Join(", ",arr)}]\n");
+CopyArray(arr);
+
+void CopyArray(int[] array){
+    int[] copyArray = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+        copyArray[i] = array[i];
+    Console.WriteLine($"[{String.Join(", ",copyArray)}]");
+}
+
+int[] GetArray(int size, int minValue = 0, int maxValue = 10){
+    int[] array = new int[size];
+    for(int i = 0; i < size; i++)
+        array[i] = new Random().Next(minValue, maxValue+1);
+    return array;
+}
---
+++ Seminar_07/Program.cs
@@ -0,0 +1,124 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите кол-во строк массива:");
+int rows = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите кол-во столбцов массива:");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArrayRandom(rows, columns, 0, 9);
+PrintArray(array);
+Console.WriteLine();
+// GetDiagonalSum(array, rows, columns);
+// GetArrayPowIJ(array, rows, columns);
+// GetSortCol(array, rows, columns);
+GetAvgSumCol(array, rows, columns);
+
+
+// ------------------------------ Method ----------------------------------
+
+// Метод создания двумерного массива Random
+int[,] GetArrayRandom(int m, int n, int minValue, int maxValue){
+    int[,] result = new int[m, n];
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            result[i,j] = new Random().Next(minValue, maxValue+1);
+        }
+    }
+    return result;
+}
+
+// Метод печати массива
+void PrintArray(int[,] array){
+    for(int i = 0; i < array.GetLength(0); i++){
+        for (int j = 0; j < array.GetLength(1); j++){
+            Console.Write(array[i,j].ToString().PadRight(6));
+        }
+    Console.WriteLine();
+    }
+}
+void PrintArrayFloat(float[] array){
+    for(int i = 0; i < array.Length; i++){
+        Console.Write(array[i].ToString().PadRight(6));
+    }
+}
+
+
+// Работа с массивом
+// Задать двумерный массив и отсортировать нечетные столбцы по возрастанию
+void GetSortCol(int[,] result, int m, int n){
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j+=2){
+            for (int g = 0; g < m-1 ; g++){
+                if (result[g,j] > result[g+1,j]){
+                    int temp = result[g,j];
+                    result[g,j] = result[g+1,j];
+                    result[g+1,j] = temp;
+                }
+            }
+        }
+    }
+    PrintArray(result);
+}
+
+
+// Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+
+// int[,] GetArrayIplusJ(int m, int n, int minValue, int maxValue){
+//     int[,] result = new int[m, n];
+//     for(int i = 0; i < m; i++){
+//         for(int j = 0; j < n; j++){
+//             result[i,j] = i+j;
+//         }
+//     }
+//     return result;
+// }
+
+
+
+// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+
+// void GetArrayPowIJ(int[,] result, int m, int n){
+//     for(int i = 0; i < m; i++){
+//         for(int j = 0; j < n; j++){
+//             if (i % 2 == 0 && j % 2 == 0) result[i, j] = (int)Math.Pow(result[i, j],2);
+//             Console.Write(result[i,j].ToString().PadLeft(4));
+//         }
+//     Console.WriteLine();
+//     }
+// }
+
+
+
+// Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+// void GetDiagonalSum(int[,] result, int m, int n){
+//     int sum = 0;
+//     for(int i = 0; i < m; i++){
+//         for(int j = 0; j < n; j++){
+//             if (i == j) sum += result[i,j];
+//         }
+//     }
+//     Console.WriteLine($"Сумма диагонали (с индексами (0,0); (1;1) и т.д) = {sum}");
+// }
+
+
+
+
+void GetAvgSumCol(int[,] result, int m, int n){
+    float[] mas = new float[n];
+    for(int j = 0; j < n; j++){
+        for(int i = 0; i < m; i++){
+            mas[j] += result[i,j];
+        }
+        mas[j] /= (float)n;
+    }
+    PrintArray(result);
+    Console.WriteLine();
+    Console.WriteLine("Среднее арифметическое:");
+    PrintArrayFloat(mas);
+}
